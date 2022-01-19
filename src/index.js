fad099c7d3030aef2321815d5bede4e944b757ed
Sergio Moreno <sergiomoreno@realtracksystems.com>
2022-01-19 23:15:46 +0100

    srating to implement HOC

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ import './index.css';
 import { App } from './components/App/index';
 import reportWebVitals from './reportWebVitals';
 
+// function App({greeting, name}) {
+//   return(<>
+//     <p> El APP </p>
+//     <p>{greeting}, {name}</p>
+//   </>)
+// }
+
+// function withGreetings(WrappedComponent) {
+//   return function WrappedComponentWithGreetings(greeting){
+//     return function TheRealOne() {
+//       return (<React.Fragment>
+//         <WrappedComponent greeting={greeting}/>
+//       </React.Fragment>)
+//     } 
+//   }
+// }
+// const AppWithGreeting = withGreetings(App)("heyy")
+
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     <App/>
   </React.StrictMode>,
   document.getElementById('root')
